6f27f5404b680ee5b8af714b29331a3f0cfb29d0
kinokotakenoko9 <chdmitry2@gmail.com>
2025-04-26 22:04:56 +0300

    fix: config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.alias['node:child_process'] = false;
+      config.resolve.alias['node:fs'] = false;
+      config.resolve.alias['node:constants'] = false;
+      config.resolve.alias['node:tty'] = false;
+    }
+    return config;
+  },
+
 };
 
 export default nextConfig;
